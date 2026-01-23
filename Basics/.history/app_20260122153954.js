b00366ea2ae59f5be2287705c31fede210efaf0e
Robin <robin.webdev.me@gmail.com>
2026-01-23 20:43:53 +0200

    Delete Basics/.history directory

--- Basics/.history/app_20260122153954.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-
-const app = express(); // Creating a server
-
-app.listen(3000);
--- Basics/.history/app_20260122154124.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express(); // Creating a server
-
-app.get("/", () => {
-    
-});
-
-app.listen(3000); // Starting the server on port 3000
