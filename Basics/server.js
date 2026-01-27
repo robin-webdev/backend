cce852f8b1c283d4d7c4685177551cc0679371e6
robin-webdev <robin.webdev.me@gmail.com>
2026-01-27 21:52:27 +0200

    connection with db

--- Basics/server.js
+++ Basics/server.js
@@ -1,7 +1,18 @@
 // import app from "./src/app.js";
-import app from "./src/database-app";
+import app from "./src/database-app.js";
+import mongoose from "mongoose";
 
-// This file is for running the server....
+// This file is for running the server.... and connecting to server......
+
+async function connectToDb() {
+  let connection = await mongoose.connect(
+    "mongodb+srv://robin:password%40db@demo-cluster.bqstfaw.mongodb.net/db-1",
+  );
+
+  console.log("Connected");
+}
+ 
+connectToDb();
 
 app.listen(3000, () => {
   console.log("Server started on https://localhost:3000/");
