f6c5cc9db03c7fac92f925249aeb20f55d4bdec9
robin-webdev <robin.webdev.me@gmail.com>
2026-02-27 23:47:46 +0200

    Just commit it

--- Project-1/src/config/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-async function connectToDb() {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("Successfully connected to Database...");
-}
-
-export default connectToDb;
